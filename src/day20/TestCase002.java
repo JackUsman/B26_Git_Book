5f55def3584a7cc14293f18b47485ec00f8f7917
JackUsman <intelmodus@gmail.com>
2022-03-29 21:31:19 -0400

    changes continue

--- src/day20/TestCase002.java
+++ src/day20/TestCase002.java
@@ -2,9 +2,14 @@ package day20;
 
 public class TestCase002 {
 
-    // setting up test case 2
+// setting up test case 2
 
-    // testing for manus
+// testing for manus
 
-    // are the clicklalbe
+// are the clicklalbe
+
+
+// doing some more changes
+
+}
 
